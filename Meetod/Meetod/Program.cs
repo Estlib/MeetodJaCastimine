b354e425ee2b7f35336f30fd7bc45baf4555ff53
Estlib <75094683+Estlib@users.noreply.github.com>
2025-11-17 09:13:26 +0200

    lisasin näite parameetritega meetodist

---
+++ Meetod/Meetod/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<string> ostunimekiri = new List<string>();
+        Console.WriteLine("Sisesta oma tänane poeskäigunimekiri");
+        string kasutajasisestus = "";
+
+        GetUserInput(kasutajasisestus, ostunimekiri);
+
+        Console.WriteLine("See on sinu nimekiri:\n");
+        foreach (var söök in ostunimekiri)
+        {
+            Console.WriteLine($" -*- {söök}");
+        }
+
+        GetUserInput(kasutajasisestus, ostunimekiri);
+    }
+
+    static List<string> GetUserInput(string kasutajasisestus, List<string> ostunimekiri)
+    {
+        while (kasutajasisestus == "")
+        {
+            Console.WriteLine("Palun järgmine ost nimekirja, kirjuta \"rohkem pole\" et lõpetada:");
+            kasutajasisestus = Console.ReadLine();
+            if (kasutajasisestus == "rohkem pole")
+            {
+                break;
+            }
+            ostunimekiri.Add(kasutajasisestus);
+            kasutajasisestus = "";
+        }
+        return ostunimekiri;
+    }
+
+}
