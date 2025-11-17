ee5170df3c2c922b3898a9c0af3dadcc9c8b633b
Estlib <75094683+Estlib@users.noreply.github.com>
2025-11-17 09:31:20 +0200

    meetodi siganatuur

--- Meetod/Meetod/Program.cs
+++ Meetod/Meetod/Program.cs
@@ -2,6 +2,7 @@
 {
     private static void Main(string[] args)
     {
+        NewMessage();
         List<string> ostunimekiri = new List<string>();
         Console.WriteLine("Sisesta oma tänane poeskäigunimekiri");
         string kasutajasisestus = "";
@@ -32,5 +33,21 @@
         }
         return ostunimekiri;
     }
+
+    //static - staatiline selles klassis
+
+    //private - meetod on saadaval ainult selles klassis
+    //public - meetod on saadaval ükskõik millises klassis
+    //protected - meetod on saadaval ainult selles klassis, või teistes klassides mis pärib selle klassi andmed
+    //internal - on saadaval ainult iseenda sees.
+
+    //signatuuri kompositsioon:
+    //1. juurdepääsu modifikaator, 2. andme tagastustüüp, 3. meetodi nimi, 4. sulud, kus võivad olla parameetrid, 
+    static                         void                   NewMessage       ()
+    { //5. teostatav koodiplokk
+        Console.WriteLine("NewMessage");
+    }
+
+
 
 }
