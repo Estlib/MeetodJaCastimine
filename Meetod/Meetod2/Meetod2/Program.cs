07dfa1ff23e19984a753ddd54fe3b6a5fc0ff518
Estlib <75094683+Estlib@users.noreply.github.com>
2025-11-24 09:35:04 +0200

    meetodite kasutusnäidis
    main programm on nüüd lihtsamini loetav

--- Meetod/Meetod2/Meetod2/Program.cs
+++ Meetod/Meetod2/Meetod2/Program.cs
@@ -4,7 +4,56 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Tervitus();
+            string kasutajaNimi = "";
+            kasutajaNimi = NimeKüsimine(kasutajaNimi);
+
+            Console.WriteLine("Mis on sinu nädalane eelarve?");
+            float eelarve = 0.00f;
+            bool kaskasutajanõustub = false;
+            kaskasutajanõustub = Eelarve(kasutajaNimi, eelarve, kaskasutajanõustub);
+
         }
+
+        private static bool Eelarve(string kasutajaNimi, float eelarve, bool kaskasutajanõustub)
+        {
+            while (kaskasutajanõustub != true)
+            {
+                Console.WriteLine($"Praegune eelarve on {eelarve}, kas see on õige?");
+                Console.WriteLine("Vasta kas jah või ei:");
+                string mistaütles = Console.ReadLine();
+
+                if (mistaütles == "jah")
+                {
+                    kaskasutajanõustub = true;
+                }
+            }
+            Console.WriteLine($"{kasutajaNimi}sinu eelarve on:{SinuEelarvePäevas(eelarve)}");
+            return kaskasutajanõustub;
+        }
+
+        private static string NimeKüsimine(string kasutajaNimi)
+        {
+            while (kasutajaNimi == "")
+            {
+                Tervitus();
+                Console.WriteLine("Palun sisesta oma kasutajanimi");
+                kasutajaNimi = Console.ReadLine();
+            }
+
+            return kasutajaNimi;
+        }
+
+        public static void Tervitus()
+        {
+            Console.WriteLine("'ommik");
+        }
+        //hallingux4 /janterx3
+        public static int SinuEelarvePäevas(float eelarve)
+        {
+            double eelarvepäevas = eelarve / 7;
+            return (int)eelarvepäevas;
+        }
+
     }
 }
